3df305efa643645ef2e7d3d3ad7d8d8377310dcb
DevangChy <DevangChy@users.noreply.github.com>
2025-01-27 17:28:25 +0530

    Class Management System

---
+++ Class Management System/Class Management System/class_System/monitor.py
@@ -0,0 +1,99 @@
+# Monitor Management System
+
+import mysql.connector as mydb
+
+# connection to the database
+conn = mydb.connect(host="localhost", user="root", password="Devang@2004", database="class")
+cur = conn.cursor()
+
+# Function to get student roll number
+def get_roll():
+    while True:
+        try:
+            roll = int(input("Enter Students Roll No: "))
+            return roll
+        except ValueError:
+            print("Invalid input. Please enter a valid roll number.")
+
+# Function to get student name
+def get_name():
+    return input("Enter Students Name: ")
+
+# Function to get student monitor month
+def get_months():
+    return input("Enter Student Month for monitor Position: ")
+
+# Function to add Monitor Details
+def add_monitor():
+    roll = get_roll()
+    name = get_name()
+    months = get_months()
+    data = (roll, name, months)
+    query = "INSERT INTO monitor (roll, name, months) VALUES (%s, %s, %s)"
+    cur.execute(query, data)
+    conn.commit()
+    print("Monitor Details Added Successfully!")
+
+# Function to show Monitor Details
+def show_monitor():
+    roll = get_roll()
+    query = "SELECT * FROM monitor WHERE roll= %s"
+    cur.execute(query, (roll,))
+    monitor = cur.fetchone()
+    if monitor:
+        print("Monitor Details")
+        print(f"Roll No.: {monitor[0]}")
+        print(f"Name: {monitor[1]}")
+        print(f"months: {monitor[2]}")
+    else: 
+        print("Monitor not found.")
+
+# Function to Update Monitor Details
+def update_monitor():
+    print("Enter Roll no. for update monitor details: ")
+    roll = get_roll()
+    print("Enter Details That can be update")
+    name = get_name()
+    months = get_months()
+    data = (name, months, roll)
+    query = "UPDATE monitor SET name = %s, months = %s WHERE roll = %s"
+    cur.execute(query, data)
+    conn.commit()
+    print("Monitor Details Update Successfully!")
+
+# Function to Delete Monitor Details
+def delete_monitor():
+    print("Enter Roll no. for delete monitor details: ")
+    roll = get_roll()
+    query = "DELETE FROM monitor WHERE roll = %s"
+    cur.execute(query, (roll,))
+    conn.commit()
+    print("Monitor Details Delete Successfully!")
+
+# Main Function
+def main():
+    while True:
+        print("Monitor Details")
+        print("1. Add Monitor Details")
+        print("2. Show Monitor Details")
+        print("3. Update Monitor Details")
+        print("4. Delete Monitor Details")
+        print("5. Exit")
+
+        choice = input("Enter Your Choice: ")
+        if choice == "1":
+            add_monitor()
+        elif choice == "2":
+            show_monitor()
+        elif choice == "3":
+            update_monitor()
+        elif choice == "4":
+            delete_monitor()
+        elif choice == "5":
+            break
+        else:
+            print("Invalid Choice. Please try again.")
+
+# Excecution of Main Function
+if __name__ == "__main__":
+    main()
---
+++ Class Management System/Class Management System/class_System/student.py
@@ -0,0 +1,119 @@
+# Student Management System
+
+import mysql.connector as mydb
+
+# connection to the database
+conn = mydb.connect(host="localhost", user="root", password="Devang@2004", database="class")
+cur = conn.cursor()
+
+# Function to get student roll number
+def get_roll():
+    while True:
+        try:
+            roll = int(input("Enter Students Roll No: "))
+            return roll
+        except ValueError:
+            print("Invalid input. Please enter a valid roll number.")
+
+# Function to get student name
+def get_name():
+    return input("Enter Students Name: ")
+
+# Function to get student email
+def get_email():
+    return input("Enter Students Email: ")
+
+# Function to get student phone number
+def get_phone():
+    while True:
+        try:
+            phone = int(input("Enter Students Phone No: "))
+            return phone
+        except ValueError:
+            print("Invalid input. Please enter a valid phone number.")
+
+# Function to get student address
+def get_address():
+    return input("Enter Students Address: ")
+
+# Function to add student details in database
+def add_student():
+    roll = get_roll()
+    name = get_name()
+    email = get_email()
+    phone = get_phone()
+    address = get_address()
+    data = (roll, name, email, phone, address)
+    query = "INSERT INTO student (roll, name, email, phone, address) VALUES (%s, %s, %s, %s, %s)"
+    cur.execute(query, data)
+    conn.commit()
+    print("Student Details Added Successfully!")
+
+# Function to show student details in database
+def show_student():
+    roll = get_roll()
+    query = "SELECT * FROM student WHERE roll = %s"
+    cur.execute(query, (roll,))
+    student = cur.fetchone()
+    if student:
+        print("Student Details:")
+        print(f"Roll No.: {student[0]}")
+        print(f"Name: {student[1]}")
+        print(f"Email: {student[2]}")
+        print(f"Phone: {student[3]}")
+        print(f"Address: {student[4]}")
+    else:
+        print("Student not found.")
+
+# Function to update student details in database
+def update_student():
+    print("Enter Roll no. for change data: ")
+    roll = get_roll()
+    print("Enter data ")
+    name = get_name()
+    email = get_email()
+    phone = get_phone()
+    address = get_address()
+    data = (name, email, phone, address, roll)
+    query = "UPDATE student SET name = %s, email = %s, phone = %s, address = %s WHERE roll = %s"
+    cur.execute(query, data)
+    conn.commit()
+    print("Student Details Updated Successfully!")
+    
+
+# Function to delete student details in database
+def delete_student():
+    print("Enter Roll no. for Delete data: ")
+    roll = get_roll()
+    query = "DELETE FROM student WHERE roll = %s"
+    cur.execute(query, (roll,))
+    conn.commit()
+    print("Student Details Deleted Successfully!")
+    
+
+# Main function
+def main():
+    while True:
+        print("Student Details")
+        print("1. Add Students Details")
+        print("2. Show Students Details")
+        print("3. Update Students Details")
+        print("4. Delete Students Details")
+        print("5. Exit")
+        choice = input("Enter Your Choice: ")
+        if choice == "1":
+            add_student()
+        elif choice == "2":
+            show_student()
+        elif choice == "3":
+            update_student()
+        elif choice == "4":
+            delete_student()
+        elif choice == "5":
+            break
+        else:
+            print("Invalid Choice. Please try again.")
+
+# Excecution of Main Function
+if __name__ == "__main__":
+    main()
